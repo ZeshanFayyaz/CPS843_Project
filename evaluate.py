306c51ab4085006bc02aa6e4adb24a87fd45c1ed
Zeshan Fayyaz <43391249+ZeshanFayyaz@users.noreply.github.com>
2021-12-15 02:57:55 -0500

    Update and rename 320x320.py to evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[262]:
-
-
 import os
 import cv2
 import numpy as np
@@ -28,10 +22,6 @@ from skimage.color import rgb2ycbcr
 from pywt import dwt2,idwt2
 import PIL.Image as Image
 
-
-# In[263]:
-
-
 image_size = 256
 
 def create_data(datadir):
@@ -46,19 +36,11 @@ def create_data(datadir):
             pass
     return training_validation_data
 
-
-# In[264]:
-
-
 def myPSNR(tar_img, new_list):
     imdff = np.float32(prd_img) - np.float32(tar_img)
     rmse = np.sqrt(np.mean(imdff**2))
     ps = 20*np.log10(255/rmse)
     return ps
-
-
-# In[265]:
-
 
 def create_dataORG(TESTDIR):
     target_data = []
@@ -74,34 +56,18 @@ def convert2RGB(image_list):
     for i in range (len(image_list)):
         image_list[i] = cv2.cvtColor(image_list[i], cv2.COLOR_RGB2BGR)
 
-
-# In[266]:
-
-
 MODEL4CNN3_13GRU8CNN = "C:\\Users\\zesha\\Desktop\\CPS_PROJECT\\model_checkpoint.h5"
 our_model = load_model(MODEL4CNN3_13GRU8CNN)
-
-
-# In[289]:
-
 
 targetTEST = create_dataORG("C:\\Users\\zesha\\Downloads\\Test100\\target\\") #original sized images. change create_dataORG to create_data
 inputTEST = create_data("C:\\Users\\zesha\\Downloads\\Test100\\input\\") #resized images
 input_2 = create_dataORG("C:\\Users\\zesha\\Downloads\\Test100\\input\\")
-
-
-# In[292]:
-
 
 input2_FIXED = []
 for i in range(len(input_2)):
     input2_FIXED.append(input_2[i][0])
     
 convert2RGB(input2_FIXED)
-
-
-# In[268]:
-
 
 target_FIXED = []
 for i in range(len(targetTEST)):
@@ -114,23 +80,11 @@ for i in range(len(inputTEST)):
 convert2RGB(input_FIXED)
 convert2RGB(target_FIXED)
 
-
-# In[294]:
-
-
 target_FIXED = np.array(target_FIXED)
 input_FIXED = np.array(input_FIXED)
 input2_FIXED = np.array(input2_FIXED)
 
-
-# In[272]:
-
-
 OUTPUT = our_model.predict(input_FIXED)
-
-
-# In[275]:
-
 
 upsampled_images = []
 for i in range(len(OUTPUT)):
@@ -140,29 +94,12 @@ for i in range(len(OUTPUT)):
     temp = temp.astype(np.uint8)
     upsampled_images.append(temp)
 
-
-# In[276]:
-
-
 new_list = upsampled_images
-
-
-# In[277]:
-
-
 img_files_tar = target_FIXED
 img_files_prd = new_list  
 
-
-# In[278]:
-
-
 print(img_files_tar[0].shape)
 print(img_files_prd[0].shape)
-
-
-# In[279]:
-
 
 psnr_mpr= []
 ssim_mpr = []
@@ -176,23 +113,11 @@ for tar_img,prd_img in zip(img_files_tar,img_files_prd):
     psnr_mpr.append(PSNR_mpr)
     ssim_mpr.append(SSIM_mpr)
 
-
-# In[280]:
-
-
 PSNR = sum(psnr_mpr)/len(psnr_mpr)
 print(PSNR)
 
-
-# In[281]:
-
-
 SSIM = sum(ssim_mpr)/len(ssim_mpr)
 print(SSIM)
-
-
-# In[318]:
-
 
 fig, ax = plt.subplots(2,3, sharey =True, figsize=(16,7))
 #Plot 1: Clean Image
@@ -218,4 +143,3 @@ ax[1,1].axis('off')
 #Plot 3: Predicted OUR_NETWORK Image 
 ax[1,2].imshow(img_files_prd[12].astype(np.uint8))
 ax[1,2].axis('off')
-
